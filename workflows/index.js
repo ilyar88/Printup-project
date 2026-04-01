d76887e822776990f82a69e739f1017c5a1c6c27
ilyar88 <irahmilevich@gmail.com>
2026-04-02 01:15:19 +0300

    Add comments to index.js file.

--- workflows/index.js
+++ workflows/index.js
@@ -1,3 +1,6 @@
+// Gathers all workflow classes into one object and exports them together,
+// so other files can import multiple workflows from a single path (e.g. require('./workflows'))
+// instead of requiring each workflow file individually.
 module.exports = {
     LoginFlow: require('./LoginFlow'),
     ClientInfoFlow: require('./ClientInfoFlow'),
