bbc80b5e3a3b2e46c6656c744b02dbec7c10bba1
ilyar88 <irahmilevich@gmail.com>
2026-04-02 16:09:15 +0300

    Add changes to the  MaterialsInfoFlow class.

--- workflows/MaterialsInfoFlow.js
+++ workflows/MaterialsInfoFlow.js
@@ -2,7 +2,7 @@
 const MaterialsInfo = require('../pageObjects/MaterialsInfo');
 const ClientInfo = require('../pageObjects/ClientInfo');
 const UploadFiles = require('../pageObjects/UploadFiles');
-const { click, selectOption, isChecked } = require('../fixtures/User interface');
+const { click, selectOption, isChecked, uploadFile } = require('../fixtures/User interface');
 const { readExcel } = require('../TDD/ExcelReader');
 
 // Automates the Materials Info page by uploading a file, selecting material options, and saving.
@@ -17,12 +17,12 @@ class MaterialsInfoFlow {
         const clientInfo = new ClientInfo(page);
         await uploadFile(uploadFiles.uploadFile(), data.Project_path);
         await selectOption(materialsInfo.materialType(), 'value', data.Material_type);
-        await selectOption(materialsInfo.thickness(), 'value', data.Thickness);
+        await selectOption(materialsInfo.thickness(), 'value', data.Thickness_mm);
         await selectOption(materialsInfo.color(), 'value', data.Color);
         await selectOption(materialsInfo.textureMaterial(), 'value', data.Texture_material);
         await selectOption(materialsInfo.materialType_2(), 'value', data.Material_type_2);
-        await click(materialsInfo.categoryMaterials(data.Category_materials));
-        await isChecked(materialsInfo.keepPermanent());
+        await materialsInfo.categoryMaterials([data.Category_materials]);
+        await isChecked(materialsInfo.keepPermanent(), data.Keep_in_system);
         await click(materialsInfo.save());
         await click(clientInfo.nextButton());
     }
